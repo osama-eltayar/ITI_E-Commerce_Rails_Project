a8078f4a4251fc7c4d5f1f1cac5bb63bcbaedbd6
Mahmoud-64 <mahmoudyoussef070@gmail.com>
2020-05-10 23:16:54 +0200

    test added

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2020_05_06_032158) do
     t.bigint "category_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "store_id"
+    t.bigint "store_id", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["store_id"], name: "index_products_on_store_id"
@@ -127,7 +127,6 @@ ActiveRecord::Schema.define(version: 2020_05_06_032158) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
-  add_foreign_key "products", "stores"
   add_foreign_key "shopping_carts", "orders"
   add_foreign_key "shopping_carts", "products"
   add_foreign_key "shopping_carts", "users"
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -4,4 +4,10 @@ class CategoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "category should not save without title" do
+    category = Category.new
+    category.name="aa"
+    assert category.save
+  end
 end
